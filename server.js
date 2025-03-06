37ce238e7bcaf858479a12962f2571781a124f94
alexgaaranes <aaaranes@up.edu.ph>
2025-03-06 18:24:55 +0800

    feat: post request add-book implemented

--- server.js
+++ server.js
@@ -18,18 +18,62 @@ app.get('/', (req, res) => {
 
 // POST endpoint at /add-book
 app.post('/add-book', (req, res) => {
-    res.send(`Received ISBN: ${req.body.isbn}`);
+    console.log(`Received ISBN: ${req.body.isbn}`);
+
+    // Initialize add status object
+    let status = { success: false };
+
+    // Check if the book obj contains all info
+    if (req.body.bookName == undefined ||
+        req.body.isbn == undefined ||
+        req.body.author == undefined ||
+        req.body.year == undefined
+    )   res.send(status);
+    else{
+        // Read ./books.txt
+        try{
+            const data = readFileSync('./books.txt', 
+            { encoding: 'utf-8', flag: 'r'});
+
+            let isUnique = true;
+            // Check if ISBN exists
+            data.split('\n').forEach(entry =>{
+                if(entry.split(',')[1] == req.body.isbn)
+                    isUnique = false;
+            });
+            isUnique?addBook(status, req, res):res.send(status);
+            
+        } catch (err){
+            appendFileSync('./books.txt', '');  // create file
+            addBook(status, req, res);
+        }
+    }
 });
 
 // GET endpoint for /find-by-isbn-author
-app.get('find-by-isbn-author', (req, res) => {
+app.get('/find-by-isbn-author', (req, res) => {
     console.log(req.query);
 });
 
 // GET endpoint for /find-by-author
-app.get('find-by-author', (req, res) => {
+app.get('/find-by-author', (req, res) => {
     console.log(req.query);
 })
 
 // Start the server at port 3000
 app.listen(port, () => { console.log(`Server started at port ${port}`); });
+
+
+// Other Functions
+const addBook = (status, req, res) => {
+    // Save the book details in books.txt
+    let bookDetails = `${req.body.bookName},${req.body.isbn},${req.body.author},${req.body.year}\n`;
+    try{
+        appendFileSync("./books.txt", bookDetails);
+        status.success = true;
+    } catch(err) {  // if an error occurred
+        console.log(err);
+    } finally{      // send add status
+        res.send(status);
+    }
+}
--- test.js
+++ test.js
@@ -4,12 +4,12 @@ import needle from 'needle';
 needle.post(
     'http://localhost:3000/add-book',
     {
-        bookName: 'The Little Price', 
-        isbn: '978-0156012195',
-        author: 'Antoine Saint-Exupery',
-        year: 1943
+        bookName: "Harry Potter and the Chamber of Secrets", 
+        isbn: '0-7475-3849-2',
+        author: 'J.K Rowling',
+        year: 1997
     },
     (err, res) => {
-     console.log(res.body);
+     err?console.log(err):console.log(res.body);
     }
 )
